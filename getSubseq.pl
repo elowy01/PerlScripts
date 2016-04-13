1f9a8544e44ac866939c403006ab1c7474c04e37
Ernesto Lowy <ernesto@ebi.ac.uk>
2016-04-13 18:35:06 +0100

    adding modifications to getSubseq.pl

--- getSubseq.pl
+++ getSubseq.pl
@@ -1,17 +1,32 @@
 #utility to get a subseq from a fasta file containing a unique fasta sequence
 use strict;
 use warnings;
+use Getopt::Long;
 use Bio::SeqIO;
 use Bio::Seq;
 
-die("usage: getSubseq.pl <file.fa> <start> <end> <output>\n") if scalar(@ARGV<3);
+my ($file,$coords,$output,$help);
 
-my $file=$ARGV[0];
-my $start=$ARGV[1];
-my $end=$ARGV[2];
-my $output=$ARGV[3];
+my $usage = "
+  [--help]          this menu
+   --file           Fasta file with sequences to be sliced
+   --coords         Coords for fragment to be extracted. Ex: chr4:100000-102000
+   --output         Name of output file
+   [USAGE] perl $0 --file file.fa --coords chr4:100000-102000 --output subfile.fa
+";
 
-my $prefix=$1 if $file=~/(.+)\.fa/;
+&GetOptions(
+    'file=s'              => \$file,
+    'coords=s'           => \$coords,
+    'output=s'            => \$output,
+    'help'               => \$help
+    );
+
+if ($help || !$file || !$coords || !$output) {
+    print $usage;
+    exit 0;
+}
+
 
 #create SeqIO object from file.fa
 my $in=Bio::SeqIO->new(-file => "$file",
@@ -20,10 +35,15 @@ my $in=Bio::SeqIO->new(-file => "$file",
 my $out = Bio::SeqIO->new(-file => ">$output",
                        -format => 'Fasta');
 
+my ($chr,$start,$end)=($1,$2,$3) if $coords=~/(.+)\:(\d+)-(\d+)/;
+die("[ERROR] $coords coords are not valid") unless $chr && $start && $end;
+
 while (my $seq = $in->next_seq) {
+    next unless $seq->id eq $chr;
     my $subseq=$seq->subseq($start,$end);
-    my $subseqOb=Bio::Seq->new(-display_id=>"$prefix:$start-$end",
+    my $subseqOb=Bio::Seq->new(-display_id=>$coords,
 			       -seq=>$subseq);
     $out->write_seq($subseqOb);
+    last;
 }
 
